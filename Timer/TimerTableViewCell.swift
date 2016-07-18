a223261a725506a733e47c3f6728941bdff565b2
Hyunkyu Lee <hkl0902@gmail.com>
2016-07-18 14:38:55 -0700

    Adds files the Timer TVC

---
+++ Timer/TimerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TimerTableViewCell.swift
+//  Timer
+//
+//  Created by Hyunkyu Lee on 7/18/16.
+//  Copyright © 2016 Hyunkyu Lee. All rights reserved.
+//
+
+import UIKit
+
+class TimerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
