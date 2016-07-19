7f3604bad0051026adad04626d20b2af40081532
Hyunkyu Lee <hkl0902@gmail.com>
2016-07-19 15:19:40 -0700

    Adds Project when button is pressed

--- Timer/AddNewProjectTableViewCell.swift
+++ Timer/AddNewProjectTableViewCell.swift
@@ -7,10 +7,18 @@
 //
 
 import UIKit
+import CoreData
 
 class AddNewProjectTableViewCell: UITableViewCell {
 
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var AddProjectButton: UIButton!
+    var context: NSManagedObjectContext?
+    
+    @IBAction func addProject() {
+        context?.performBlockAndWait { [unowned self] in
+            Project.createProjectWithName(self.textField.text!, inManagedObjectContext: self.context!)
+        }
+    }
     
 }
