e536d620166df1db9bd8f8cf75432422e927f276
Hyunkyu Lee <hkl0902@gmail.com>
2016-07-19 14:12:09 -0700

    Refractors code

--- Timer/AppDelegate.swift
+++ Timer/AppDelegate.swift
@@ -14,6 +14,79 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        let appDel = UIApplication.sharedApplication().delegate as! AppDelegate
+        let context = appDel.managedObjectContext
+        let fetchRequest = NSFetchRequest(entityName: Project.Names.Entity)
+        
+        //deleteCoreData()
+        
+        // Set the Project.defaultID if not set already
+        if context.countForFetchRequest(fetchRequest, error: nil) == 0 {
+            context.performBlockAndWait {
+                if let project = Project.createProjectWithName(Project.Names.DefaultProject, inManagedObjectContext: context) {
+                    Project.defaultID = project.id
+                    print("Default ID Set ", terminator: Project.defaultID! + "\n")
+                }
+            }
+            // Save the Project ID
+            if let projectID = Project.defaultID {
+                let fileManager = NSFileManager()
+                if let documentsDirectory = fileManager.URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask).first {
+                    if !fileManager.fileExistsAtPath(documentsDirectory.absoluteString + Project.Names.defaultFile) {
+                        let defaultIDData = projectID.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
+                    }
+                }
+            }
+            
+            do {
+                try context.save()
+            } catch let error {
+                print("\(error)")
+            }
+            
+        } else {
+            let fileManager = NSFileManager()
+            if let documentsDirectory = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first {
+                do {
+                    try Project.defaultID = NSString(contentsOfFile: documentsDirectory.path! + Project.Names.defaultFile, encoding: NSUTF8StringEncoding) as String
+                } catch {
+                    print("File not read")
+                }
+                
+            }
+        }
+        
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        // Saves changes in the application's managed object context before the application terminates.
+        self.saveContext()
+    }
+
+    // MARK: - Core Data stack
+    
     func deleteCoreData() {
         let appDel = UIApplication.sharedApplication().delegate as! AppDelegate
         let context = appDel.managedObjectContext
@@ -46,82 +119,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             debugPrint(error)
         }
     }
-
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        let appDel = UIApplication.sharedApplication().delegate as! AppDelegate
-        let context = appDel.managedObjectContext
-        let fetchRequest = NSFetchRequest(entityName: Project.Names.Entity)
-        
-        // deleteCoreData()
-        
-        // Set the Project.defaultID if not set already
-        if context.countForFetchRequest(fetchRequest, error: nil) == 0 {
-            context.performBlockAndWait {
-                if let project = Project.createProjectWithName(Project.Names.DefaultProject, inManagedObjectContext: context) {
-                    Project.defaultID = project.id
-                    print("Default ID Set ", terminator: Project.defaultID! + "\n")
-                }
-            }
-            // Save the Project ID
-            if let projectID = Project.defaultID {
-                let fileManager = NSFileManager()
-                if let documentsDirectory = fileManager.URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains: NSSearchPathDomainMask.UserDomainMask).first {
-                    if !fileManager.fileExistsAtPath(documentsDirectory.absoluteString + Project.Names.defaultFile) {
-                        let defaultIDData = projectID.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false)
-                        let fileCreated = fileManager.createFileAtPath(documentsDirectory.path! + Project.Names.defaultFile, contents: defaultIDData, attributes: nil)
-                    }
-                }
-            }
-            
-            do {
-                try context.save()
-            } catch let error {
-                print("\(error)")
-            }
-            
-        } else {
-            let fileManager = NSFileManager()
-            if let documentsDirectory = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first {
-                do {
-                    try Project.defaultID = NSString(contentsOfFile: documentsDirectory.path! + Project.Names.defaultFile, encoding: NSUTF8StringEncoding) as String
-                } catch {
-                    print("File not read")
-                }
-            }
-        }
-        return true
-    }
-
-    func applicationWillResignActive(application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
-        
-        
-        
-        self.saveContext()
-    }
-
-    // MARK: - Core Data stack
 
     lazy var applicationDocumentsDirectory: NSURL = {
         // The directory the application uses to store the Core Data store file. This code uses a directory named "just.for.fun.Timer" in the application's documents Application Support directory.
