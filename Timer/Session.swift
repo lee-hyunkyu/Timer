13d12407a74493a4171564f04e774d8b27919b20
Hyunkyu Lee <hkl0902@gmail.com>
2016-07-21 12:13:44 -0700

    Fixes boolean test so Sessions are created when a timer is not active, not when it is

--- Timer/Session.swift
+++ Timer/Session.swift
@@ -14,7 +14,7 @@ class Session: NSManagedObject {
     
     class func createSessionToTimer(timer: Timer, inManagedObjectContext context: NSManagedObjectContext) -> Session? {
         // Only create a new session if the timer isn't active.
-        if timer.isActive as! Bool {
+        if !(timer.isActive as! Bool) {
             if let newSession = NSEntityDescription.insertNewObjectForEntityForName(Names.Entity, inManagedObjectContext: context) as? Session {
                 newSession.id = NSUUID().UUIDString
                 newSession.startTime = NSDate()
